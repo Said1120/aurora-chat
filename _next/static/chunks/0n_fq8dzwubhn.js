232313637ddafb8bf962dd0537e0829c641fccd0
Bowen <bowen@student.local>
2026-07-25 10:29:53 +0800

    Updates

---
+++ _next/static/chunks/0n_fq8dzwubhn.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,85167,r=>{"use strict";var e=r.i(90499);r.s(["PwaRegister",0,function(){return(0,e.useEffect)(()=>{"serviceWorker"in navigator&&navigator.serviceWorker.register("/aurora-chat/sw.js",{scope:"/aurora-chat/"}).catch(()=>void 0)},[]),null}])}]);
